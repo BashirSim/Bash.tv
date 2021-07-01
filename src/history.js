588c746a7bbed671d954c79469ec9c36ef28d17a
BashirSim <44641488+BashirSim@users.noreply.github.com>
2021-07-01 12:18:04 +0300

    Update history.js

--- src/history.js
+++ src/history.js
@@ -1,4 +1,4 @@
 import { createBrowserHistory } from "history";
-// This package is installed with react-router-dom
+// This package is installed with react-router-dom.
 
 export default createBrowserHistory();
